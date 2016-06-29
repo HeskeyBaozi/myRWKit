2976d67b8beae13512d86bc6ff04ed6bca8a4c4e
HeskeyBaozi <hezhiyu971014@foxmail.com>
2016-06-29 14:02:36 +0800

    修正静态属性

--- src/component/TodoApp.jsx
+++ src/component/TodoApp.jsx
@@ -15,16 +15,25 @@ class TodoApp extends React.Component {
             <Footer/>
         </div>);
     }
-
 }
 
-// 默认属性外部定义
+/*
+* ES6明确规定，Class内部只有静态方法，没有静态属性。
+* 所以以下两种写法都无效
+* class Foo {
+*     // 写法一
+*     prop: 2
+*     // 写法二
+*     static prop: 2
+*}
+*
+* Foo.prop // undefined
+* ES7有一个静态属性的提案，目前Babel转码器支持
+* */
 TodoApp.defaultProps = {
     title: 'World!'
 };
 
-TodoApp.propTypes = {
-    title: React.PropTypes.string.isRequired
-};
+TodoApp.propTypes = {};
 
 export default TodoApp;
